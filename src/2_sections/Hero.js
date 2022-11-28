bf9cdf330bf45d91d14e913b264129fc0e49634c
Paolo Todd <iampaolotodde@gmail.com>
2022-11-28 15:31:48 +0000

    Update

--- src/2_sections/Hero.js
+++ src/2_sections/Hero.js
@@ -9,7 +9,7 @@ function Hero() {
 
    return (
       <section className="bg-primary">
-         <div className="w-[80%] text-left pt-32 md:pt-72 pb-16">
+         <div className="md:w-[80%] text-left pt-32 md:pt-72 pb-16">
             <TransitionInview>
                <h1 className="mb-8 sm:mb-12 md:mb-16 lg:mb-20 leading-less xl:leading-tight">
                   {" "}
