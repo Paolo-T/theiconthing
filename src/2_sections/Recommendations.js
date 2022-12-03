52e2868e3dfd74bc9e90879cc39d445b2deea92e
Paolo Todd <iampaolotodde@gmail.com>
2022-12-03 11:43:56 +0000

    update

--- src/2_sections/Recommendations.js
+++ src/2_sections/Recommendations.js
@@ -62,7 +62,7 @@ function Reccomendations() {
             <p>From amazingly talented humans</p>
          </div>
 
-         <div className="w-full col-start-1 col-end-17 md:col-end-13">
+         <div className="w-full col-start-1 col-end-17 lg:col-end-13">
             <div className="sm:grid grid-cols-2 sm:grid-gap place-content-stretch text-center">
                {reccomendations.map((recommendation) => (
                   <TransitionInview key={recommendation.id}>
