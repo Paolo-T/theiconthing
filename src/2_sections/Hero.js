351288c5a1b04baaefc90ddbd7a194135332466b
Paolo Todd <iampaolotodde@gmail.com>
2022-12-16 09:53:35 +0100

    update

--- src/2_sections/Hero.js
+++ src/2_sections/Hero.js
@@ -6,11 +6,13 @@ function Hero() {
    const location = useLocation()
    return (
       <section className="relative w-full pt-32 md:pt-64 spacing-bottom text-center xl:mb-32">
-         <span className="absolute inset-0 bg-ImageBg bg-auto w-full h-full" style={{ opacity: "4%" }}></span>
-         <span
-            className="absolute inset-0 z-0 bg-gradient-to-t from-dark to-transparent"
-            style={{ opacity: "90%" }}
-         ></span>
+         <div>
+            <span className="absolute inset-0 bg-ImageBg bg-auto w-full h-full" style={{ opacity: "4%" }}></span>
+            <span
+               className="absolute inset-0 z-0 bg-gradient-to-t from-dark  via-dark/80 to-transparent"
+               style={{ opacity: "90%" }}
+            ></span>
+         </div>
          <TransitionInview>
             <div className="md:grid grid-cols-16 grid-gap">
                <h1 className="col-start-4 col-end-14 mb-8 sm:mb-12 md:mb-16 lg:mb-16 leading-some ">
