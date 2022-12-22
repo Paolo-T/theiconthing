120e457eb2a7b20453cc96763efda272080909c5
Paolo Todd <iampaolotodde@gmail.com>
2022-12-22 14:52:55 +0100

    update

--- src/2_sections/Hero.js
+++ src/2_sections/Hero.js
@@ -2,6 +2,11 @@ import React from "react"
 import { useLocation } from "@reach/router"
 import TransitionInview from "../1_components/TransitionInview"
 
+const strings = {
+   hero: "Human centred design solutions.",
+   subtitle: "UI | UX | Code",
+}
+
 function Hero() {
    const location = useLocation()
    return (
@@ -15,12 +20,9 @@ function Hero() {
          </div>
          <TransitionInview>
             <div className="md:grid grid-cols-16 grid-gap">
-               <h1 className="col-start-4 col-end-14 mb-8 sm:mb-12 md:mb-16 lg:mb-16 leading-some ">
-                  {" "}
-                  Human centred design solutions.
-               </h1>
+               <h1 className="col-start-4 col-end-14 mb-8 sm:mb-12 md:mb-16 lg:mb-16 leading-some "> {strings.hero}</h1>
             </div>
-            <h2 className="text-sm md:text-base font-bold uppercase tracking-widest">UI | UX | Code </h2>
+            <h2 className="text-sm md:text-base font-bold uppercase tracking-widest">{strings.subtitle}</h2>
          </TransitionInview>
       </section>
    )
