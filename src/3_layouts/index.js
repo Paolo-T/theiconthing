1dc8c35ed8db81cb754e536163369d116fe4bd9c
Paolo Todd <iampaolotodde@gmail.com>
2022-12-23 23:17:18 +0100

    update

--- src/3_layouts/index.js
+++ src/3_layouts/index.js
@@ -2,12 +2,12 @@ import PropTypes from "prop-types"
 import React from "react"
 import Navigation from "../2_sections/Navigation"
 import Footer from "../2_sections/Footer"
-// import CookieConsent from "src/1_components/Cookie-banner.js"
+import CookieConsent from "../1_components/Cookie-banner.js"
 
 function Layout({ children }) {
    return (
       <div className="w-full relative">
-         {/* <CookieConsent /> */}
+         <CookieConsent />
          <Navigation />
          <main className="w-full">{children}</main>
          <Footer />
