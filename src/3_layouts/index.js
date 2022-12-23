7401d29064390b0e12ead2b213cbea6ab3b9ca2f
Paolo Todd <iampaolotodde@gmail.com>
2022-12-23 22:49:20 +0100

    update

--- src/3_layouts/index.js
+++ src/3_layouts/index.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types"
 import React from "react"
 import Navigation from "../2_sections/Navigation"
 import Footer from "../2_sections/Footer"
-import CookieConsent from "../1_components/Cookie-banner"
+import CookieConsent from "./1_components/Cookie-banner.js"
 
 function Layout({ children }) {
    return (
