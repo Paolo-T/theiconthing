87c1d0af7da8661b64759c013a3603032746499e
Paolo Todd <iampaolotodde@gmail.com>
2023-01-22 21:40:50 +0000

    Update

--- src/3_layouts/index.js
+++ src/3_layouts/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import Navigation from "../2_sections/Navigation"
 import Footer from "../2_sections/Footer"
